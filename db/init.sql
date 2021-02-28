2a0c2250cfe83ef17c1351ef1d31dcbd868a6c89
KarimHabush <37211852+KarimHabush@users.noreply.github.com>
2021-02-28 16:17:57 +0100

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -2,3 +2,5 @@ CREATE DATABASE students_manager;
 use students_manager;
 
 CREATE TABLE students(id int unsigned not null auto_increment, name varchar(100) not null, mark double not null, primary key (id));
+                                                                                                                              
+INSERT INTO students (name, mark) VALUES ('Karim', 15),  ('TST', 13);
