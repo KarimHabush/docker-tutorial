d4744b47f68e2d9c4dc6ab2b9994c21626f7b2e6
KarimHabush <37211852+KarimHabush@users.noreply.github.com>
2021-02-28 17:35:39 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request,redirect, url_for
 import requests
 import json
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 @app.route('/')
 def homepage():
     msg=""
